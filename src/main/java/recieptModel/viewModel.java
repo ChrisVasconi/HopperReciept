fd052d0b01de954cf17857a957133c1ff7e517c6
Chris Vasconi <vasconichris@gmail.com>
2022-08-07 12:20:24 -0400

    First commit

---
+++ src/main/java/recieptModel/viewModel.java
@@ -0,0 +1,5 @@
+package recieptModel;
+
+public class viewModel {
+
+}
---
+++ src/test/java/com/chrisvasconi/hoppersreciept/HoppersRecieptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chrisvasconi.hoppersreciept;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoppersRecieptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
